3409e2380b6e37e0483418cfecf02d027ed39da4
3ncrypter <hi@surajverma.in>
2017-11-22 12:02:48 +0530

    first update for bitbucket

--- js/script.js
+++ js/script.js
@@ -22,3 +22,20 @@ $(function(){
     
 });
 
+(function(){
+
+  var parallax = document.querySelectorAll(".parallax"),
+      speed = 0.3;
+
+  window.onscroll = function(){
+    [].slice.call(parallax).forEach(function(el,i){
+
+      var windowYOffset = window.pageYOffset,
+          elBackgrounPos = "0 " + '-' + (windowYOffset * speed) + "px";
+      
+      el.style.backgroundPosition = elBackgrounPos;
+
+    });
+  };
+
+})();
